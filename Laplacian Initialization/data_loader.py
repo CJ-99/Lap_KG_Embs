fc2680830fa59fc20c215faf344b351ad3cd4644
Conor Chalcroft <71027000+CJ-99@users.noreply.github.com>
2022-09-16 14:57:26 +0100

    Add files via upload

---
+++ Laplacian Initialization/data_loader.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import ampligraph
+from ampligraph.datasets import load_wn18rr
+# import torch
+# from torch.geometric.datasets import WordNet18RR
+
+LOG_DIR = './logs'
+DATA_PATH = './data'
